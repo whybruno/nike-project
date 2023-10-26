fac0d5808a647f75ff8bd3e108509c733781be37
whybruno.dev@gmail.com <whybruno.dev@gmail.com>
2023-10-26 19:31:58 +0100

    super-quality section done

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -14,13 +14,19 @@ const Hero = () => {
       className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-        <p className="text-xl font-montserrat text-coral-red">Out Summer Collection</p>
+        <p className="text-xl font-montserrat text-coral-red">
+          Out Summer Collection
+        </p>
         <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
-          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10"> 
+            The New Arrival
+          </span>
           <br />
-          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
+          <span className="text-coral-red inline-block mt-3">Nike </span> Shoes
         </h1>
-        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
+          Discover stylish Nike arrivals, quality comfort, and innovation for your active life.
+        </p>
         <Button
           label="Shop now"
           iconURL={arrowRight}
@@ -28,8 +34,12 @@ const Hero = () => {
         <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
           {statistics.map((stat) => (
             <div key={stat.label}>
-              <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
-              <p className="leading-7 font-montserrat text-slate-gray">{stat.label}</p>
+              <p className="text-4xl font-palanquin font-bold">
+                {stat.value}
+              </p>
+              <p className="leading-7 font-montserrat text-slate-gray">
+                {stat.label}
+              </p>
             </div>
           ))}
         </div>
@@ -47,11 +57,11 @@ const Hero = () => {
         <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
           {shoes.map((shoe) => (
             <div key={shoe}>
-                <ShoeCard
-                  imgURL={shoe}
-                  changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
-                  bigShoeImg={bigShoeImg}
-                />
+              <ShoeCard
+                imgURL={shoe}
+                changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
+                bigShoeImg={bigShoeImg}
+              />
             </div>
           ))}
         </div>
