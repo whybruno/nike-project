64fdd5560673b833c69cbcd2b91c262b9d47cc63
whybruno.dev@gmail.com <whybruno.dev@gmail.com>
2023-10-27 13:36:04 +0100

    customer-reviews and subscribe section done

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -6,7 +6,7 @@ const Services = () => {
     <section className="max-container flex justify-center flex-wrap gap-9">
       {services.map((service) => (
         <ServiceCard key={service.label} {...service} />
-      ))};
+      ))}
     </section>
   );
 };
